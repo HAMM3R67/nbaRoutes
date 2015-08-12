b7990e7a803600769b3f16689a56f9fd72ad2f35
HAMM3R67 <tcschauer@gmail.com>
2015-08-11 20:53:47 -0600

    working on the post method

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -4,12 +4,17 @@ app.service('teamService', function($http, $q){
 
 this.addNewGame = function(gameObj){
 	var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+	
 	parseInt(gameObj.homeTeamScore);
 	parseInt(gameObj.opponentScore);
+	
 	if(gameObj.homeTeamScore > gameObj.opponentScore){
 		gameObj.won = true;
 	}else{
 		gameObj.won = false;
+	}
+	$http.post('url', data){
+		return data;
 	}
 }
 
