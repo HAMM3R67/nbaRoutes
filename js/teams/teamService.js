2dfb9806328ef819b50d466e83ac6eef64f2e7f9
HAMM3R67 <tcschauer@gmail.com>
2015-08-11 20:44:59 -0600

    added if statement for win or not

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,4 +2,13 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
 
+this.addNewGame = function(gameObj){
+	var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+	if(gameObj.homeTeamScore > gameObj.opponentScore){
+		gameObj.won = true;
+	}else{
+		gameObj.won = false;
+	}
+}
+
 });
