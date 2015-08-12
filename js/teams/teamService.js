1e8bd15bfe8d86f0b5790e5a70fa24e954c6e3ac
HAMM3R67 <tcschauer@gmail.com>
2015-08-11 20:56:58 -0600

    working on the getTeamData method

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -3,19 +3,24 @@ var app = angular.module('nbaRoutes');
 app.service('teamService', function($http, $q){
 
 this.addNewGame = function(gameObj){
-	var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
-	
-	parseInt(gameObj.homeTeamScore);
-	parseInt(gameObj.opponentScore);
-	
-	if(gameObj.homeTeamScore > gameObj.opponentScore){
-		gameObj.won = true;
-	}else{
-		gameObj.won = false;
+		var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+		
+		parseInt(gameObj.homeTeamScore);
+		parseInt(gameObj.opponentScore);
+		
+		if(gameObj.homeTeamScore > gameObj.opponentScore){
+			gameObj.won = true;
+		}else{
+			gameObj.won = false;
+		}
+		$http.post('url', data){
+			return data;
+		}
 	}
-	$http.post('url', data){
-		return data;
-	}
-}
+	
+this.getTeamData = function(team){
+	var deferred = $q.defer()
+	
+}	
 
 });
