114776920ce55751d19653921cd3865dac572b28
HAMM3R67 <tcschauer@gmail.com>
2015-08-11 20:49:17 -0600

    added a couple parseInt

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -4,6 +4,8 @@ app.service('teamService', function($http, $q){
 
 this.addNewGame = function(gameObj){
 	var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+	parseInt(gameObj.homeTeamScore);
+	parseInt(gameObj.opponentScore);
 	if(gameObj.homeTeamScore > gameObj.opponentScore){
 		gameObj.won = true;
 	}else{
